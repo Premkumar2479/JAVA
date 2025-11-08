202d4fa6cb2bda2450a3b96b40aa13d2e6f08537
Premkumar2479 <pk2335301@gmail.com>
2025-11-08 12:09:32 +0530

    some text

---
+++ Satement/condition.java
@@ -0,0 +1,17 @@
+Import java.util.*;
+
+package Satement;
+
+public class condition {
+    public sataic void main(String[] args){
+        Scanner sc = new Scanner (System.in);  
+        int x = sc.nextint();
+
+        if (x%2==0){
+            System.out.println("even number");
+        }
+        else{
+            System.out.println("odd number");
+        }
+    }
+}
