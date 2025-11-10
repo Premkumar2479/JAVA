f2f29f367f081da83a13802951bf1be1e199cfea
Premkumar2479 <pk2335301@gmail.com>
2025-11-10 21:08:59 +0530

    some text

--- Satement/condition.java
+++ Satement/condition.java
@@ -1,17 +1,17 @@
-Import java.util.*;
-
-package Satement;
+import java.util.Scanner;
 
 public class condition {
-    public sataic void main(String[] args){
-        Scanner sc = new Scanner (System.in);  
-        int x = sc.nextint();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);  
+        System.out.print("Enter a number: ");
+        int x = sc.nextInt();  // Corrected: nextInt(), not nextint()
 
-        if (x%2==0){
-            System.out.println("even number");
+        if (x % 2 == 0) {
+            System.out.println("Even number");
+        } else {
+            System.out.println("Odd number");
         }
-        else{
-            System.out.println("odd number");
-        }
+
+        sc.close(); // Always good to close the scanner
     }
 }
