4725da9866b6bfe2d22662b44a8ca62570b6dd7e
Premkumar2479 <pk2335301@gmail.com>
2025-11-12 14:15:59 +0530

    add text

--- Satement/Conditions.java
+++ Satement/Conditions.java
@@ -1,30 +1,30 @@
 import java.util.Scanner;
 
-public class SwitchE {
+public class Conditions {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);  
+        Scanner sc = new Scanner (System.in);
 
-        System.out.print("Enter a number (1-7): ");
+        System.out.println("Enter the number (1-7): " );
         int day = sc.nextInt();
 
-        switch(day){
+        switch(day)
+        {
             case 1 : System.out.println("Sunday");
                 break;
             case 2 : System.out.println("Monday");
                 break;
             case 3 : System.out.println("Tuesday");
                 break;
-            case 4 : System.out.println("Wednesday");
+            case 4 : System.out.println("wednesday");
                 break;
             case 5 : System.out.println("Thursday");
-                break;  
+                break;
             case 6 : System.out.println("Friday");
                 break;
-            case 7 : System.out.println("Saturday");
+            case 7 : System.out.println("saturday");
                 break;
-            default : System.out.println("Invalid day");
+            default :System.out.println("Invaild day");
         }
         sc.close();
     }
-    
 }
