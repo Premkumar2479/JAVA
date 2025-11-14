29302d89da4d700cfc6b43ecc3291a31e25efdbe
Premkumar2479 <pk2335301@gmail.com>
2025-11-14 15:18:57 +0530

    add text

---
+++ Fuctions/Name.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Name {
+
+    public static void printName(String name){
+        system.out.println(name);
+        return ;
+    }
+
+
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        String name = sc.nextLine();
+
+        printName(name);
+
+
+    }
+}
