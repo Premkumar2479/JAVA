faa03cd2597c81cb05e4b35f21b0b2159c2103c3
Premkumar2479 <pk2335301@gmail.com>
2025-11-24 23:20:41 +0530

    some text

---
+++ String/Practice2.java
@@ -0,0 +1,8 @@
+public class Practice2 {
+    public static void main(String[] args){
+        String firstname = "tony";
+        String lastname = "stack";
+        String fullname = firstgname + lastname ;
+        System.out.println(fullname);
+    }
+}
