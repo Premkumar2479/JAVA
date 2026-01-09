812fa3e03e75c5a5c0d2571100f580a39937aa20
Premkumar2479 <pk2335301@gmail.com>
2026-01-09 12:43:03 +0530

    some text

---
+++ CodeChef/Ques14.java
@@ -0,0 +1,11 @@
+public class Ques14 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        name = scanner.nextLine();
+        System.out.println(name.length());
+
+        scanner.close();
+    }
+    
+}
