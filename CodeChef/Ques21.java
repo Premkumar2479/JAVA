0fcffb849b6d17e60abfc6d00a0f6731df7ce299
Premkumar2479 <pk2335301@gmail.com>
2026-01-10 23:48:51 +0530

    add text

---
+++ CodeChef/Ques21.java
@@ -0,0 +1,11 @@
+public class Ques21
+ {
+    public static void main(String[] args) {
+       int marks = 75;
+       char grade;
+       if (marks >= 60)
+         grade = 'A';
+       else
+         grade = 'B';
+    }
+}
