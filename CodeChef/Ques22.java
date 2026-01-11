1d81518a2ddf438dae4006718c986387c5c150ca
Premkumar2479 <pk2335301@gmail.com>
2026-01-11 20:29:52 +0530

    push files

---
+++ CodeChef/Ques22.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Ques22 {
+    public static void main(String[] args){
+        int x=10;
+        int y=5;
+        System.out.println(x>5 && y<3);
+    }
+}
