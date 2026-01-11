d951959a9d68a5e8153e696a848eb6fe02137b8e
Premkumar2479 <pk2335301@gmail.com>
2026-01-11 20:38:04 +0530

    some text

---
+++ CodeChef/Ques23.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Ques23 {
+    public static void main(String[] args){
+        Boolean a=false;
+        Boolean b=false;
+        Boolean c=false;
+        System.out.println((a && b) || (c||a));
+    }
+}
