d61694b68a8ebf436ed4147e5759ca938d33d9c8
Premkumar2479 <pk2335301@gmail.com>
2026-01-12 23:36:53 +0530

    add files

---
+++ CodeChef/Ques29.java
@@ -0,0 +1,7 @@
+public class Ques29 {
+  public static void main(String[] args) {
+    int x = 10;
+    int y = 5;
+    System.out.println(x > 5 && y < 3);
+  }
+}
---
+++ CodeChef/Ques30.java
@@ -0,0 +1,9 @@
+public class Ques30 {
+  public static void main(String[] args) {
+    boolean a = false;
+    boolean b = true;
+    boolean c = false;
+    
+    System.out.println((a && b) || (c || a));
+  }
+}
