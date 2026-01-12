9a4b7457332cfba546573768cd9f40e1702643a8
Premkumar2479 <pk2335301@gmail.com>
2026-01-12 23:15:57 +0530

    some text

---
+++ CodeChef/Ques26.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ques26 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        if (x>5 && y<10){
+            System.out.println("in Range");
+        }else{
+            System.out.println("out of Range");
+        }
+    }
+}
