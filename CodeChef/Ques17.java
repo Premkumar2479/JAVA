3d567ec13e91ea92b5db1256a651517b6e02f888
Premkumar2479 <pk2335301@gmail.com>
2026-01-14 21:46:17 +0530

    add push

--- CodeChef/Ques17.java
+++ CodeChef/Ques17.java
@@ -2,5 +2,10 @@
 
 
 public class Ques17 {
-    
+    public static void main(String[] args){
+        double radius = 5.0;
+        double pi = 3.14;
+        double area = pi * radius * radius;
+        System.out.println("Area of the circle: " +area);
+    } 
 }
