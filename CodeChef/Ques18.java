02cadae74f4750bab22f79f4b64337c13ec1467b
Premkumar2479 <pk2335301@gmail.com>
2026-01-14 21:41:29 +0530

    add files

--- CodeChef/Ques18.java
+++ CodeChef/Ques18.java
@@ -1,5 +1,9 @@
 // Write a program that takes the two different strings as input and prints them in a single line separated by spaces.
 
 public class Ques18 {
-    
+    public static void main(String[] args){
+        String str1 = "Hello";
+        String str2 = "World";
+        System.out.println(str1 + " " + str2);
+    }
 }
