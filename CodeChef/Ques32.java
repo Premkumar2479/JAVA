18f3338de53c09308175887bcfbff4c9682be54a
Premkumar2479 <pk2335301@gmail.com>
2026-01-16 23:39:34 +0530

    edit files

---
+++ CodeChef/Ques32.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ques32 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int total = a*b*100;
+        System.out.println(total);
+
+        sc.close();
+    }
+}
