f76764dc4f6d98aaa4a8345ee94f1058e79cc38d
Premkumar2479 <pk2335301@gmail.com>
2026-01-18 23:53:25 +0530

    some test

---
+++ CodeChef/Ques35.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ques35 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            int total = (x*30)+(y*50);
+
+            System.out.println(total);
+            
+        
+    }
+}
