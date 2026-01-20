a4ea37e73a7cdb79aca1a9754132ea84349d3a6b
Premkumar2479 <pk2335301@gmail.com>
2026-01-20 22:58:30 +0530

    some text

---
+++ CodeChef/Ques38.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ques38 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        if(A==B){
+            System.out.print("yes");
+        }else{
+            System.out.print("no");
+        }
+    }
+}
