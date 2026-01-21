34b271184a12ef0449c71f21cd947a1ef2e92d27
Premkumar2479 <pk2335301@gmail.com>
2026-01-21 22:44:39 +0530

    some text

---
+++ CodeChef/Ques40.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Ques40 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int z = sc.nextInt();
+
+            int L = Math.max(x - y, x - z);
+            int R = Math.min(x + y, x + z);
+
+            int count = Math.max(0, R - L + 1);
+
+            // Exclude Chef himself
+            if (L <= x && x <= R) {
+                count--;
+            }
+
+            System.out.println(count);
+        }
+    }
+}
