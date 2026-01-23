e102acef3343f3a34963eabe2ad687dce89b8f27
Premkumar2479 <pk2335301@gmail.com>
2026-01-23 23:09:58 +0530

    some text

---
+++ CodeChef/Ques44.java
@@ -0,0 +1,8 @@
+public class Ques44 {
+  // main method
+  public static void main(String[] args)
+  {
+    // Output: Hello, world!
+    System.out.println("Hello, world!");
+  }
+}
---
+++ CodeChef/Ques45.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ques45 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int cost = Math.min(1000, 200 * N);
+        System.out.println(cost);
+    }
+}
