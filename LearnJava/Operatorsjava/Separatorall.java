0c69c06cbe1c2bd8b7f3d5527ab5f63c60f4e3e2
Premkumar2479 <pk2335301@gmail.com>
2026-01-28 23:59:07 +0530

    push commit

---
+++ LearnJava/Operatorsjava/Separatorall.java
@@ -0,0 +1,14 @@
+package LearnJava.;
+
+public class Separatorall {
+    public static void main(String[] args){
+    // Separators: Separators are symbols that define the structure of Java programs by separating different code elements.
+
+    // Parentheses ( ):      Used to enclose method parameters or control flow statements.
+    // Curly braces { }:     Used to define blocks of code, such as in methods or classes.
+    // Square brackets [ ]:  Used for array declarations.
+    // Semicolon ;:          Used to end statements.
+    // Comma ,:              Used to separate items in a list.
+    // Period .:             Used to access object members or package names.
+    }
+}
---
+++ LearnJava/Scopeinjava/Scope.java
@@ -0,0 +1,26 @@
+package LearnJava.Scopeinjava;
+
+public class Scope {
+    public static void main(String[]args){
+
+    //Common Escape Sequences in Java
+
+//  \n - Newline: Moves the cursor to the next line.
+    System.out.println("Hello\nWorld");  // Output:    Hello
+                                           //            World
+//  \t - Tab: Adds a tab space.
+    System.out.println("Name:\tJohn");  //Output: Name:    John
+
+
+//  \\ - Backslash: Used to print a backslash character.
+    System.out.println("C:\\Program Files"); //Output: C:\Program Files
+
+
+//  \" - Double quote: Used to print a double quote within a string.
+    System.out.println("She said, \"Hello!\""); //Output: She said, "Hello!"
+
+
+//  \' - Single quote: Used to print a single quote.
+    System.out.println("It\'s a beautiful day"); //Output: It's a beautiful day
+}
+}
