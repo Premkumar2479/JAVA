a3898fc9d696b9b49df516c57316288b20182ac6
Premkumar2479 <pk2335301@gmail.com>
2026-01-28 23:27:24 +0530

    some text

---
+++ LearnJava/SimpleQues/output/literal/Literalchar.java
@@ -0,0 +1,12 @@
+package LearnJava.SimpleQues.output.literal;
+
+//Character Literals: Character literals represent a single character and are enclosed 
+// in single quotes ('). For Example:
+
+public class Literalchar {
+    public static void main(String[] args) {
+        System.out.println('A');  // Character literal
+        System.out.println('$');  // Character literal
+    }
+}
+
