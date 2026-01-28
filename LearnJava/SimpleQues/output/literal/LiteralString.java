4f5e1aabe09d53cb29bd7941df5795943722104f
Premkumar2479 <pk2335301@gmail.com>
2026-01-28 23:31:00 +0530

    initial project setup

---
+++ LearnJava/SimpleQues/output/literal/LiteralString.java
@@ -0,0 +1,10 @@
+package LearnJava.SimpleQues.output.literal;
+//String Literals: String literals represent a sequence of characters and are
+// c enclosed in double quotes ("). For Example:
+
+public class LiteralString {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");  // String literal
+        System.out.println("Java is fun!");   // String literal
+    }
+}
---
+++ LearnJava/SimpleQues/output/literal/Literalboolean.java
@@ -0,0 +1,10 @@
+package LearnJava.SimpleQues.output.literal;
+
+//Boolean Literals: Boolean literals represent true or false values in Java. For Example:
+
+public class Literalboolean {
+    public static void main(String[] args) {
+        System.out.println(true);   // Boolean literal
+        System.out.println(false);  // Boolean literal
+    }
+}
