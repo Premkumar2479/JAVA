47a76fe55d7e83e4947b43803ab4664708faf31c
Premkumar2479 <pk2335301@gmail.com>
2026-01-28 22:29:03 +0530

    some text

---
+++ LearnJava/SimpleQues/output/Q1.java
@@ -0,0 +1,15 @@
+package LearnJava.SimpleQues.output;
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int total = x*y;
+
+        System.out.println("Multiple of two number :" + total);
+
+    }
+}
