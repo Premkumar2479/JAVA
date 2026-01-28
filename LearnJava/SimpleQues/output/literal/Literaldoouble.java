a3250b2e84e3e921b7f9a2831d0b88ae3aad073b
Premkumar2479 <pk2335301@gmail.com>
2026-01-28 23:21:18 +0530

    mid commit

---
+++ LearnJava/SimpleQues/output/literal/Literaldoouble.java
@@ -0,0 +1,11 @@
+package LearnJava.SimpleQues.output.literal;
+
+
+//Double Literals:
+class Literaldoouble {
+    public static void main(String[] args) {
+        System.out.println(3.14);     // Double literal
+        System.out.println(0.1);      // Double literal
+        System.out.println(2.5e3);    // Double literal with scientific notation
+    }
+}
