261c05baacaee5e0bf527b7a6ce932eb5d2ef8a8
Premkumar2479 <pk2335301@gmail.com>
2026-01-28 23:18:17 +0530

    add text

---
+++ LearnJava/SimpleQues/output/literal/Liateralfloat.java
@@ -0,0 +1,12 @@
+package LearnJava.SimpleQues.output.literal;
+
+
+//Float Literals:
+public class Liateralfloat {
+      public static void main(String[] args) {
+        System.out.println(3.14f);    // Float literal
+        System.out.println(0.1f);     // Float literal
+        System.out.println(2.5e3f);   // Float literal with scientific notation
+    }
+
+}
---
+++ LearnJava/SimpleQues/output/literal/literalint.java
@@ -0,0 +1,10 @@
+package LearnJava.SimpleQues.output.literal;
+
+class literalint {
+   public static void main(String[] args) {
+       System.out.println(42);       // Decimal integer literal
+       System.out.println(052);      // Octal integer literal
+       System.out.println(0x2A);     // Hexadecimal integer literal
+   }
+}
+
