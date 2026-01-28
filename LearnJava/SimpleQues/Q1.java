78a428b79366d6e44acf7816f8f8dd708f3485e3
Premkumar2479 <pk2335301@gmail.com>
2026-01-28 22:51:41 +0530

    some text

--- LearnJava/SimpleQues/Q1.java
+++
@@ -1,7 +0,0 @@
-package LearnJava.SimpleQues;
-
-public class Q1 {
-    public static void main(String[] args){
-        System.out.println("hello world");
-    }
-}
---
+++ LearnJava/SimpleQues/output/Q2.java
@@ -0,0 +1,8 @@
+package LearnJava.SimpleQues.output;
+
+public class Q2 {
+    public static void main(String[] args){
+            System.out.println("hello world!");
+
+    }
+}
