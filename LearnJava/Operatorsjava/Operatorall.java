9cf26506551a32da6a1a6b3e8b8e77915b098a6c
Premkumar2479 <pk2335301@gmail.com>
2026-01-29 22:56:14 +0530

    some text

---
+++ LearnJava/Operatorsjava/Operatorall.java
@@ -0,0 +1,11 @@
+package LearnJava.Operatorsjava;
+
+public class Operatorall {
+    public static void main(String[] args){
+    // Operators: Operators are symbols that perform calculations and comparisons in Java.
+
+    // Arithmetic operators: +, -, *, /, %          (for addition, subtraction, multiplication, division, and modulus)
+    // Comparison operators: ==, !=, <, >, <=, >=   (compare values)
+    // Logical operators: &&, ||, !                 (for AND, OR, and NOT operations)
+    }
+}
