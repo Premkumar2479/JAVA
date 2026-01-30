0b1c4dc548c3f5e7ebdf00ee4d2960437324ad26
Premkumar2479 <pk2335301@gmail.com>
2026-01-30 23:46:32 +0530

    add files

---
+++ LearnJava/Datatype/Double1.java
@@ -0,0 +1,17 @@
+package LearnJava.Datatype;
+
+public class Double1 {
+    public static void main(String[] args) {
+        // Step 1: Declare and assign temperature (float)
+        float x = 28.9f;
+        double y = 125.6789;
+
+        // Step 2: Declare and assign rainfall level (double)
+
+
+        // Step 3: Print the weather data
+        System.out.println("Temperature: " + x + " °C");
+        System.out.println("Rainfall Level: " + y+ " mm");
+
+    }
+}
