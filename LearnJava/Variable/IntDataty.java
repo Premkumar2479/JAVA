6aaa78954b190dcd17583a73bd95dbece466ea7d
Premkumar2479 <pk2335301@gmail.com>
2026-01-30 23:19:07 +0530

    some text

---
+++ LearnJava/Variable/IntDataty.java
@@ -0,0 +1,15 @@
+package LearnJava.Variable;
+
+public class IntDataty {
+    public static void main(String[] args) {
+        // Declare and assign population values
+        int cityPopulation = 5000000;
+        short districtPopulation =25000;
+
+
+        // Print the population values
+
+        System.out.println("City Population:" + cityPopulation);
+        System.out.println("District Population:" +districtPopulation);
+    }
+}
