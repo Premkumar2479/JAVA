5f12d0baf9a21673dc8e110daae21f65d1938ce3
Premkumar2479 <pk2335301@gmail.com>
2026-01-31 21:42:37 +0530

    some text

---
+++ LearnJava/Datatype/Int3.java
@@ -0,0 +1,12 @@
+package LearnJava.Datatype;
+
+public class Int3 {
+    public static void main(String[] args){
+        int y= 24;
+        long o = 56L;
+
+        System.out.println("hello :" + y);
+        System.out.println("world :"+ o);
+
+    }
+}
