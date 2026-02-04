efd40445aefdfe9113ff4b709b0c2382e54abd71
Premkumar2479 <pk2335301@gmail.com>
2026-02-04 16:22:04 +0530

    some text

---
+++ QuesPractice/Q1.java
@@ -0,0 +1,11 @@
+package QuesPractice;
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        
+    }
+}
