fe7762d2671f18835166bb752b6c659962dc680b
Premkumar2479 <pk2335301@gmail.com>
2026-02-05 10:35:57 +0530

    some text

--- QuesPractice/Q1.java
+++ QuesPractice/Q1.java
@@ -6,6 +6,9 @@ public class Q1 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-        
+        int y = sc.nextInt();
+
+        int Total = x*y;
+        System.out.println("Enter two values:" + Total);
     }
 }
