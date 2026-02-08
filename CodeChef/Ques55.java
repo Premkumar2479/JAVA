25704a7c337893d022fd8d0ce683649b0b1f3068
Premkumar2479 <pk2335301@gmail.com>
2026-02-08 22:01:44 +0530

    some text

---
+++ CodeChef/Ques55.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Ques55 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+
+        if (N >= 2 * X && N >= 2 * Y) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
