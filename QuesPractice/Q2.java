e3369e689d442d13a353e64cd1352cce83e02869
Premkumar2479 <pk2335301@gmail.com>
2026-02-09 23:23:38 +0530

    add text

---
+++ QuesPractice/Q2.java
@@ -0,0 +1,16 @@
+package QuesPractice;
+
+import java.util.Scanner;
+
+public class Q2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        int total = x+y;
+        System.out.println(total);
+
+    }
+
+}
