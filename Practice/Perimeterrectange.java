06d972a89cd81a891c92b22cb5fd189e5d2026c7
Premkumar2479 <pk2335301@gmail.com>
2026-03-17 20:30:20 +0530

    some updates

---
+++ Practice/Perimeterrectange.java
@@ -0,0 +1,17 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Perimeterrectange {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        int width = sc.nextInt();
+
+        int Perimeter = 2*(length + width);
+
+        System.out.println("Perimter of rectangle : "  + Perimeter);
+
+        sc.close();
+    }
+}
